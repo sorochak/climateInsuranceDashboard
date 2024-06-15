6d67966556095d92d650e1238486866ead8d7f4d
Austen Sorochak <austensorochak@gmail.com>
2024-06-15 10:15:45 -0700

    add necessary libraries

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Container, Box, Typography } from "@mui/material";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .post("http://localhost:5000/adjust", {
+        // include any required POST data here
+      })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+      });
+  }, []);
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md">
+      <Box>
+        <Typography variant="h4" align="center">
+          Welcome to the Climate Insurance Dashboard
+        </Typography>
+      </Box>
+    </Container>
   );
 }
 
